fc72ca3659917362e2848a85cd029f47ad366875
voiddevv <brotales26@gmail.com>
2022-12-04 16:51:30 -0500

    i forgor setup scripts

---
+++ setup/setup.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+haxelib git flixel https://github.com/YoshiCrafter29/flixel
+haxelib install flixel-ui
+haxelib git openfl https://github.com/openfl/openfl
+haxelib git hscript-improved https://github.com/YoshiCrafter29/hscript-improved
+haxelib install lime
+haxelib run lime setup
---
+++ setup/setup.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+haxelib git flixel https://github.com/YoshiCrafter29/flixel
+haxelib install flixel-ui
+haxelib git openfl https://github.com/openfl/openfl
+haxelib git hscript-improved https://github.com/YoshiCrafter29/hscript-improved
+haxelib install lime
+haxelib run lime setup
+echo "note thet if you use flixel 5.0 it may not build so dont add issues"
